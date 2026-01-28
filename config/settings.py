388da3631411f1af6b3f02bf3bde8ee5ca5071db
Rahmatulloh <kadirovrahmatilloqodirov202@gmail.com>
2026-01-28 09:25:21 +0500

    Add dj-database-url for Render deployment

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k_w^korhdax^f%ohf-+a*dc4a$)$fqejq^tphka(e2lrl1bg74
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "shaxsiy-blog-15.onrender.com"]# Application definition
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "shaxsiy-blog-16.onrender.com"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -72,17 +72,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-import os
-from pathlib import Path
-import dj_database_url
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-# DATABASES sozlamasi
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
@@ -140,4 +134,3 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # config/settings.py
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
